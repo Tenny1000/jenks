894b12bee20d16b435503dc8a16567c6ba358a66
Tenny1000 <144987582+Tenny1000@users.noreply.github.com>
2023-09-20 17:10:27 +0530

    initial jenk.java

---
+++ jenk.java
@@ -0,0 +1,8 @@
+public class jenk {
+public static void main(String args[]) {
+System.out.println("Hello World");
+System.out.println("Hello Tenny");
+System.out.println("Hello Jenkins");
+System.out.println("Hello Jenkins");
+}
+}
